15f1ea9882f3e5e96bc739107386d61189d6ed7f
202-14\User <frogman@gmail.com>
2021-10-15 15:43:39 +0900

    [최원제]  - 학원 다모아 최초 생성

---
+++ src/test/java/com/hagwondamoa/HagwondamoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hagwondamoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HagwondamoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
